9e955102fe71b33194baf6bc644d4ea72297a9b0
KaryneSantoss <sanntoskaryne@gmail.com>
2023-11-07 14:30:05 -0300

    07.11_01

---
+++ src/environments/environment.ts
@@ -0,0 +1,35 @@
+export const environment = {
+    production: false,
+  
+    // Configurações do aplicativo.
+    appName: 'Terapia Mãos Hábeis',
+    appSlogan: 'Lorem, ipsum dolor!',
+  
+    // Tipo de login. ['redirect' ou 'popup'].
+    signInMethod: 'redirect',
+  
+    // Coleção de contatos do Firestore.
+    contactCollection: 'contacts',
+  
+    // Coleção principal do Firebase.
+    dbCollection: 'things',
+  
+    // Imagem padrão de novos documentos.
+    dbDefaultImage: '/assets/generic.png',
+  
+    // Lista de formatos suportados para as imagens dos documentos.
+    dbImageFormats: ['gif', 'jpg', 'jpeg', 'png', 'bmp', 'tiff', 'tif'],
+  
+    // Altere estas chaves conforme seu próprio projeto no Firebase.com.
+    firebase: {
+        apiKey: "AIzaSyCeNMmCHghO1wUlp0DaWp7hawAMeJTvvGQ",
+        authDomain: "final-71410.firebaseapp.com",
+        projectId: "final-71410",
+        storageBucket: "final-71410.appspot.com",
+        messagingSenderId: "270787168854",
+        appId: "1:270787168854:web:aa3b1301a6e2a985b549f7",
+        measurementId: "G-XN1FSDGJ7Q"
+    }
+  
+  };
+  
